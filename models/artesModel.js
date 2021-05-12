1d5c62f8f2a9985e66f1041ba652be400a04d670
Ricardo Cruz <56094485+ricardocruz00@users.noreply.github.com>
2021-05-12 07:38:47 +0100

    queries c/group by para artes e sessões

--- models/artesModel.js
+++ models/artesModel.js
@@ -2,7 +2,17 @@ var pool = require("./DBConn");
 
 module.exports.getAll = async function () {
     try {
-        let sql = "select nome, arte.id as arteID1, arte.longitude, arte.latitude, imagem, nome_artista FROM arte INNER JOIN sessaoFotos ON arte.id = sessaoFotos.arte_id INNER JOIN fotografia ON sessaoFotos.id = fotografia.fotografiaInfo_id INNER JOIN arte_artista ON arte_artista.arte_id = arte.id INNER JOIN artista ON artista.id = arte_artista.artista_id";
+        let sql = "select nome, fotografia.id, imagem, arte.id as arteID1, arte.longitude, arte.latitude, nome_artista "+
+        "FROM arte "+ 
+        "INNER JOIN sessaoFotos ON arte.id = sessaoFotos.arte_id "+
+        "INNER JOIN fotografia ON sessaoFotos.id = fotografia.fotografiaInfo_id "+ 
+        "INNER JOIN arte_artista ON arte_artista.arte_id = arte.id "+
+        "INNER JOIN artista ON artista.id = arte_artista.artista_id "+
+        "where fotografia.id in (select max(fotografia.id) "+
+                                 "FROM arte "+
+                                 "INNER JOIN sessaoFotos ON arte.id = sessaoFotos.arte_id "+
+                                 "INNER JOIN fotografia ON sessaoFotos.id = fotografia.fotografiaInfo_id "+
+                                 "group by arte.id )";
         let artes = await pool.query(sql);
         return { status: 200, data: artes };
     } catch (err) {
@@ -10,20 +20,7 @@ module.exports.getAll = async function () {
         return { status: 500, data: err };
     }
 }
-//select arte.id, max(fotografia.id)
-	FROM arte
-    INNER JOIN sessaoFotos ON arte.id = sessaoFotos.arte_id
-    INNER JOIN fotografia ON sessaoFotos.id = fotografia.fotografiaInfo_id
-    group by arte.id
 
-
-//select fotografia.id, imagem
-FROM fotografia
-where fotografia.id in (select max(fotografia.id)
-	                     FROM arte
-                         INNER JOIN sessaoFotos ON arte.id = sessaoFotos.arte_id
-                         INNER JOIN fotografia ON sessaoFotos.id = fotografia.fotografiaInfo_id
-                         group by arte.id )
 // module.exports.getImagemArte = async function () {
 //     try {
 //         let sql = "SELECT arte.id, fotografia.imagem, sessaoFotos.timestamp FROM arte INNER JOIN sessaoFotos ON arte.id = sessaoFotos.arte_id INNER JOIN fotografia ON sessaoFotos.id = fotografia.fotografiaInfo_id";
